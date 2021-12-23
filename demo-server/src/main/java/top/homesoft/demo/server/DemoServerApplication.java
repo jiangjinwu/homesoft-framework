50e55c87753889aced42aeb590dccea9522e3121
jjw <jiangjinwu@qq.com>
2021-12-23 10:39:42 +0800

    删除无关模块

--- demo-server/src/main/java/top/homesoft/demo/server/DemoServerApplication.java
+++
@@ -1,4 +0,0 @@
-package top.homesoft.demo.server;
-
-public class DemoServerApplication {
-}
--- syndata/src/main/java/top/homesoft/syncData/constants/DMLType.java
+++
@@ -1,5 +0,0 @@
-package top.homesoft.syncData.constants;
-
-public enum DMLType {
-    INSERT, DELETE, UPDATE;
-}
