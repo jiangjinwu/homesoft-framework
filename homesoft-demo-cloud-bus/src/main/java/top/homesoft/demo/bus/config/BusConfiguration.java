6da44051c630ab7997cf08e4399bfe14f7af8145
jjw <jiangjinwu@qq.com>
2021-12-23 11:45:02 +0800

    去掉私有信息

--- homesoft-demo-cloud-bus/src/main/java/top/homesoft/demo/bus/config/BusConfiguration.java
+++
@@ -1,9 +0,0 @@
-package top.homesoft.demo.bus.config;
-
-import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@RemoteApplicationEventScan("top.homesoft.demo.bus")
-public class BusConfiguration {
-}
--- homesoft-demo-cloud-bus/src/main/java/top/homesoft/demo/bus/envnt/StoreChengeEvent.java
+++
@@ -1,8 +0,0 @@
-package top.homesoft.demo.bus.envnt;
-
-import org.springframework.cloud.bus.event.RemoteApplicationEvent;
-
-public class StoreChengeEvent  extends RemoteApplicationEvent {
-
-
-}
